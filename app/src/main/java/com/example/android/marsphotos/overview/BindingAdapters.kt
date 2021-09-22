0ae22ddeccb2afc5c9fcec21b7cf7073114996b7
nilsonsasaki <nilsonsasaki@gmail.com>
2021-09-22 10:11:13 -0300

    added status bindingAdapter

--- app/src/main/java/com/example/android/marsphotos/overview/BindingAdapters.kt
+++ app/src/main/java/com/example/android/marsphotos/overview/BindingAdapters.kt
@@ -1,5 +1,6 @@
 package com.example.android.marsphotos.overview
 
+import android.view.View
 import android.widget.ImageView
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
@@ -23,4 +24,22 @@ fun bindRecyclerView(recyclerView: RecyclerView,
                      data: List<MarsPhoto>?) {
     val adapter = recyclerView.adapter as PhotoGridAdapter
     adapter.submitList(data)
+}
+
+@BindingAdapter("marsApiStatus")
+fun bindStatus(statusImageView: ImageView,
+               status: MarsApiStatus?) {
+    when (status) {
+        MarsApiStatus.LOADING -> {
+            statusImageView.visibility = View.VISIBLE
+            statusImageView.setImageResource(R.drawable.loading_animation)
+        }
+        MarsApiStatus.ERROR -> {
+            statusImageView.visibility = View.VISIBLE
+            statusImageView.setImageResource(R.drawable.ic_connection_error)
+        }
+        MarsApiStatus.DONE -> {
+            statusImageView.visibility = View.GONE
+        }
+    }
 }
