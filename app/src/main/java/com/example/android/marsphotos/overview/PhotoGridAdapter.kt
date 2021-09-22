04ee010b4c5ad9e98c5021f27eb012bc7f2890dd
nilsonsasaki <nilsonsasaki@gmail.com>
2021-09-22 08:58:55 -0300

    implemented todos

--- app/src/main/java/com/example/android/marsphotos/overview/PhotoGridAdapter.kt
+++ app/src/main/java/com/example/android/marsphotos/overview/PhotoGridAdapter.kt
@@ -1,6 +1,8 @@
 package com.example.android.marsphotos.overview
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.android.marsphotos.databinding.GridViewItemBinding
@@ -9,12 +11,24 @@ import com.example.android.marsphotos.network.MarsPhoto
 class PhotoGridAdapter : ListAdapter<MarsPhoto,
         PhotoGridAdapter.MarsPhotoViewHolder>(DiffCallback) {
 
+    companion object DiffCallback : DiffUtil.ItemCallback<MarsPhoto>() {
+        override fun areItemsTheSame(oldItem: MarsPhoto, newItem: MarsPhoto): Boolean {
+            return oldItem==newItem
+        }
+
+        override fun areContentsTheSame(oldItem: MarsPhoto, newItem: MarsPhoto): Boolean {
+            return oldItem.imgSrcUrl == newItem.imgSrcUrl
+        }
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MarsPhotoViewHolder {
-        TODO("Not yet implemented")
+        return MarsPhotoViewHolder(GridViewItemBinding.inflate(
+            LayoutInflater.from(parent.context)))
     }
 
     override fun onBindViewHolder(holder: MarsPhotoViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val marsPhoto = getItem(position)
+        holder.bind(marsPhoto)
     }
 
     class MarsPhotoViewHolder(
@@ -30,6 +44,7 @@ class PhotoGridAdapter : ListAdapter<MarsPhoto,
 
     }
 }
+
 
 
 
